7ae7c363e559681f0ab554b8961ce53ba9110785
Vlad <ricean.vlad7@gmail.com>
2023-07-23 18:40:52 +0300

    BindService

--- app/src/main/java/com/riviem/findmyphoneclap/core/data/service/clapdetecting/AudioClassificationServiceImpl.kt
+++ app/src/main/java/com/riviem/findmyphoneclap/core/data/service/clapdetecting/AudioClassificationServiceImpl.kt
@@ -1,15 +1,31 @@
 package com.riviem.findmyphoneclap.core.data.service.clapdetecting
 
 import android.app.NotificationManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
 import android.provider.Settings
 import javax.inject.Inject
 
 class AudioClassificationServiceImpl @Inject constructor(
     private val context: Context,
-    private val audioTFLite: AudioTFLite
 ): AudioClassificationService {
+
+    private lateinit var audioTFLite: AudioTFLite
+    private var bound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            audioTFLite = (service as AudioTFLite.LocalBinder).getService()
+            bound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            bound = false
+        }
+    }
 
     override suspend fun startService() {
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -20,9 +36,14 @@ class AudioClassificationServiceImpl @Inject constructor(
         }
         val intent = Intent(context, AudioTFLite::class.java)
         context.startForegroundService(intent)
+        context.bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
 
     override fun stopService() {
+        if (bound) {
+            context.unbindService(connection)
+            bound = false
+        }
         audioTFLite.stopService()
     }
 }
