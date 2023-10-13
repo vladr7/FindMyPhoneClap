45b8a1992ac69c2f98aae903e2328b89f662dc4b
Vlad <ricean.vlad7@gmail.com>
2023-10-13 09:55:01 +0300

    Added 2 sounds

--- app/src/main/java/com/riviem/findmyphoneclap/features/settings/enums/ChooseSound.kt
+++ app/src/main/java/com/riviem/findmyphoneclap/features/settings/enums/ChooseSound.kt
@@ -5,7 +5,9 @@ import com.riviem.findmyphoneclap.R
 enum class ChooseSound(val id: Int, val title: String, val index: Int) {
     SOUND_1(id = R.raw.ringtone2, "Classic", 1),
     SOUND_2(id = R.raw.birdwhistle, "Bird whistle", 2),
-    SOUND_3(id = R.raw.ringtone3, "Direct Wave", 3);
+    SOUND_3(id = R.raw.ringtone3, "Direct Wave", 3),
+    SOUND_4(id = R.raw.ringtonefaraway, "Far Away", 4),
+    SOUND_5(id = R.raw.ringtonebells, "Bells", 5);
 
     companion object {
         fun findByIndex(index: Int): ChooseSound {
