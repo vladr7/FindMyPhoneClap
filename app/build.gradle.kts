e27cca09e2f80d32e24a842f40b1c59ac9513209
Vlad <ricean.vlad7@gmail.com>
2023-12-01 15:45:40 +0200

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.riviem.findmyphoneclap"
         minSdk = 27
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
